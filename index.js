c74972fbb5478613f898b30017342b4591268aa0
juanangel89 <juansrangel89@gmail.com>
2025-11-12 16:24:12 -0500

    backend caht

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express")
+const app = express()
+const http = require("http")
+const cors = require("cors")
+const {Server} = require("socket.io")
+
+app.use(cors())
+
+const server = http.createServer(app)
+
+const io = new Server(server, {
+    cors: {
+        origin:"http://localhost:3000",
+        methods: ["GET", "POST"]
+    }
+})
+
+io.on("connection", (socket)=>{
+    socket.on("join_room",(data)=>{
+        socket.join(data)
+        console.log(`Usuario con id: ${socket.id} se unio a la sala: ${data}`)
+    })
+
+    socket.on("send_message", (data)=>{
+        
+        // 'data' debe coincidir con la interfaz Message del frontend:
+        // { user: string, message: string, room: string, time: string }
+        
+        console.log("Mensaje recibido:", data);
+        
+        // El método más importante: 'socket.to(room).emit(event, data)'
+        // Esto envía el mensaje a *todos* los demás en esa sala, excepto al remitente.
+        socket.to(data.roomId).emit("receive_message", data);
+        
+        // NOTA: Si quisieras enviarlo a todos (incluyendo al remitente),
+        // usarías: io.in(data.room).emit("receive_message", data);
+    })
+
+    socket.on("disconnect",()=>{
+        console.log("Usuario desconectado", socket.id);
+        
+    })
+} )
+
+server.listen(3001,()=>{
+    console.log("server running");
+    
+})
